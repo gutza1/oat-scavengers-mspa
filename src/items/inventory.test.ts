d6f3dfb0e4e4bf48933a5e65d25a6a4271cb0f90
Ruby Iris Juric <ruby@srxl.me>
2026-06-28 17:59:00 +1000

    fix inventory tests

--- src/items/inventory.test.ts
+++ src/items/inventory.test.ts
@@ -1,5 +1,10 @@
 import { beforeEach, describe, expect, test } from "vitest";
-import Inventory from "./inventory";
+import Inventory, { type Item } from "./inventory";
+
+const item: Item = {
+  id: "test item",
+  image: "test image"
+}
 
 let inventory: Inventory;
 
@@ -21,26 +26,26 @@ describe("getItemInSlot", () => {
 
 describe("addItem", () => {
 	test("adds an item to the first empty slot", () => {
-		inventory.addItemToSlot("test item", 0);
-		inventory.addItemToSlot("test item", 1);
-		inventory.addItemToSlot("test item", 5);
-		inventory.addItem("test item");
+		inventory.addItemToSlot(item, 0);
+		inventory.addItemToSlot(item, 1);
+		inventory.addItemToSlot(item, 5);
+		inventory.addItem(item);
 
-		expect(inventory.getItemInSlot(2)).toBe("test item");
+		expect(inventory.getItemInSlot(2)).toBe(item);
 	});
 
 	test("adds an item to the first slot of an empty inventory", () => {
-		inventory.addItem("test item");
+		inventory.addItem(item);
 
-		expect(inventory.getItemInSlot(0)).toBe("test item");
+		expect(inventory.getItemInSlot(0)).toBe(item);
 	});
 
 	test("throws an error if the inventory is full", () => {
 		for (let i = 0; i < 20; i++) {
-			inventory.addItemToSlot("test item", i);
+			inventory.addItemToSlot(item, i);
 		}
 
-		expect(() => inventory.addItem("test item")).toThrow(
+		expect(() => inventory.addItem(item)).toThrow(
 			"Tried to add item, but the inventory is full",
 		);
 	});
@@ -48,36 +53,39 @@ describe("addItem", () => {
 
 describe("addItemToSlot", () => {
 	test("adds an item to the specified slot", () => {
-		inventory.addItemToSlot("test item", 0);
+		inventory.addItemToSlot(item, 0);
 
-		expect(inventory.getItemInSlot(0)).toBe("test item");
+		expect(inventory.getItemInSlot(0)).toBe(item);
 	});
 
 	test("throws an out of bounds error if the slot specified is negative", () => {
-		expect(() => inventory.addItemToSlot("test item", -1)).toThrow(
+		expect(() => inventory.addItemToSlot(item, -1)).toThrow(
 			"Tried to add item to out of bounds inventory slot -1",
 		);
 	});
 
 	test("throws an out of bounds error if the slot specified is beyond the max inventory size", () => {
-		expect(() => inventory.addItemToSlot("test item", 100)).toThrow(
+		expect(() => inventory.addItemToSlot(item, 100)).toThrow(
 			"Tried to add item to out of bounds inventory slot 100",
 		);
 	});
 
 	test("throws a conflict error if the slot specified already contains an item, without overwriting the slot", () => {
-		inventory.addItemToSlot("test item", 0);
+		inventory.addItemToSlot(item, 0);
 
-		expect(() => inventory.addItemToSlot("other test item", 0)).toThrow(
+		expect(() => inventory.addItemToSlot({
+      ...item,
+      id: "other test item"
+    }, 0)).toThrow(
 			"Tried to add item to inventory slot 0 that already contains an item",
 		);
-		expect(inventory.getItemInSlot(0)).toBe("test item");
+		expect(inventory.getItemInSlot(0)).toBe(item);
 	});
 });
 
 describe("clearSlot", () => {
 	test("removes the item from the given slot", () => {
-		inventory.addItemToSlot("test item", 0);
+		inventory.addItemToSlot(item, 0);
 		inventory.clearSlot(0);
 
 		expect(inventory.getItemInSlot(0)).toBeNull();
@@ -105,38 +113,41 @@ describe("clearSlot", () => {
 
 describe("swapSlots", () => {
 	test("swaps the items in the given slots", () => {
-		inventory.addItemToSlot("item 1", 0);
-		inventory.addItemToSlot("item 2", 1);
+    const item1 = { ...item, id: "item 1" }
+    const item2 = { ...item, id: "item 2" }
+
+		inventory.addItemToSlot(item1, 0);
+		inventory.addItemToSlot(item2, 1);
 		inventory.swapSlots(0, 1);
 
-		expect(inventory.getItemInSlot(0)).toBe("item 2");
-		expect(inventory.getItemInSlot(1)).toBe("item 1");
+		expect(inventory.getItemInSlot(0)).toBe(item2);
+		expect(inventory.getItemInSlot(1)).toBe(item1);
 	});
 
 	test("moves an item to an empty slot", () => {
-		inventory.addItemToSlot("test item", 0);
+		inventory.addItemToSlot(item, 0);
 		inventory.swapSlots(0, 1);
 
 		expect(inventory.getItemInSlot(0)).toBeNull();
-		expect(inventory.getItemInSlot(1)).toBe("test item");
+		expect(inventory.getItemInSlot(1)).toBe(item);
 	});
 
 	test("throws an out of bounds error if the first slot is out of bounds, without modifying the inventory", () => {
-		inventory.addItemToSlot("test item", 0);
+		inventory.addItemToSlot(item, 0);
 
 		expect(() => inventory.swapSlots(-2, 0)).toThrow(
 			"Tried to swap item in out of bounds inventory slot -2",
 		);
-		expect(inventory.getItemInSlot(0)).toBe("test item");
+		expect(inventory.getItemInSlot(0)).toBe(item);
 	});
 
 	test("throws an out of bounds error if the second slot is out of bounds, without modifying the inventory", () => {
-		inventory.addItemToSlot("test item", 0);
+		inventory.addItemToSlot(item, 0);
 
 		expect(() => inventory.swapSlots(0, 100)).toThrow(
 			"Tried to swap item in out of bounds inventory slot 100",
 		);
-		expect(inventory.getItemInSlot(0)).toBe("test item");
+		expect(inventory.getItemInSlot(0)).toBe(item);
 	});
 
 	test("throws an out of bounds error if both slots are out of bounds", () => {
@@ -148,9 +159,9 @@ describe("swapSlots", () => {
 
 describe("itemCount", () => {
 	test("returns the amount of items in the inventory", () => {
-		inventory.addItemToSlot("test item", 1);
-		inventory.addItemToSlot("test item", 5);
-		inventory.addItemToSlot("test item", 17);
+		inventory.addItemToSlot(item, 1);
+		inventory.addItemToSlot(item, 5);
+		inventory.addItemToSlot(item, 17);
 
 		expect(inventory.itemCount).toBe(3);
 	});
